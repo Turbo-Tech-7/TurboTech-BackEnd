0802c2fb480ff6735389463506d1f485bc288abf
Jv-Torelli <joao.vmatos@sptech.school>
2026-03-31 17:57:26 -0300

    fix: Removendo crud fornecedor da main

--- garagem52/src/main/java/com/garagem52/domain/exception/fornecedor/FornecedorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.garagem52.domain.exception.fornecedor;
-
-public class FornecedorNotFoundException extends RuntimeException {
-    public FornecedorNotFoundException(String message) {
-        super(message);
-    }
-}
