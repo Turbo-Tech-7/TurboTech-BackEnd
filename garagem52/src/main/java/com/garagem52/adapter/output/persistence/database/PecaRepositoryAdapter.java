6bbb7226a4b81dce72c507fbc0994565ac3126c0
Jv-Torelli <joao.vmatos@sptech.school>
2026-05-21 09:41:33 -0300

    G52-Atualizacao_back_end - fix: Uso do mongo template para fazer uma busca do tipo like no banco, de acordo com o valor digitado.

--- garagem52/src/main/java/com/garagem52/adapter/output/persistence/database/PecaRepositoryAdapter.java
+++ garagem52/src/main/java/com/garagem52/adapter/output/persistence/database/PecaRepositoryAdapter.java
@@ -1,14 +1,19 @@
 package com.garagem52.adapter.output.persistence.database;
 
+import com.garagem52.adapter.output.persistence.entity.PecaEntity;
 import com.garagem52.adapter.output.persistence.mapper.PecaPersistenceMapper;
 import com.garagem52.adapter.output.persistence.repository.MongoPecaRepository;
 import com.garagem52.domain.model.Peca;
 import com.garagem52.ports.output.PecaOutputPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Component
@@ -17,17 +22,27 @@ public class PecaRepositoryAdapter implements PecaOutputPort {
 
     private final MongoPecaRepository repository;
     private final PecaPersistenceMapper mapper;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public List<Peca> findByNome(String nomePeca) {
-        return repository.findByNomePecaContainingIgnoreCase(nomePeca).stream()
-                .map(mapper::toDomain).collect(Collectors.toList());
+        Pattern pattern = Pattern.compile(".*" + Pattern.quote(nomePeca) + ".*",
+                Pattern.CASE_INSENSITIVE);
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("nomePeca").regex(pattern));
+
+        return mongoTemplate.find(query, PecaEntity.class)
+                .stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Peca> findByPreco(Double precoPeca) {
         return repository.findByPrecoPecaLessThanEqual(precoPeca).stream()
-                .map(mapper::toDomain).collect(Collectors.toList());
+                .map(mapper::toDomain)
+                .collect(Collectors.toList());
     }
 
     @Override
